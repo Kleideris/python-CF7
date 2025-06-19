f6cc595c407385043bf9240b7044d55e7a442d75
Ch. Kleideris <kleiderisch@gmail.com>
2025-06-20 00:03:11 +0300

    initial commit

---
+++ introduction/friend's_ds/ds_demo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# Sample data
+data = np.array([10, 20, 30, 40, 50])
+
+# Compute mean
+mean = np.mean(data)
+
+# Print Mean
+print(mean)
---
+++ introduction/friend's_ds/out_box.py
@@ -0,0 +1,9 @@
+a = 10
+
+print(a)
+
+print = 100
+
+del print
+
+print(a)
