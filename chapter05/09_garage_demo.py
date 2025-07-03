b0e4036495684dcdf2007883f80ae34f2a2b0495
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-04 02:03:44 +0300

    feat: add menu and the main logic loop with garage operations

--- chapter05/09_garage_demo.py
+++ chapter05/09_garage_demo.py
@@ -11,7 +11,7 @@ def display_garage(garage: deque) -> None:
 def add_car_to_garage(garage: deque, max_capacity: int) -> None:
     if len(garage) < max_capacity:
         car_name = input("Enter the name or ID of the car: ")
-        garage.appent(car_name)
+        garage.append(car_name)
         print(f"{car_name} has entered in the garage.")
     else:
         print("The garage is full. Cannot add more cars.")
@@ -23,10 +23,50 @@ def remove_car_from_garage(garage: deque) -> None:
     else:
         print("Garage is empty. No cars to remove.")
 
+#--------------------------
+import time
+def textDelay():
+    time.sleep(0.5)
+    print()
+#--------------------------
+    
+
 def main():
-    #TODO: menu, printmenu, match, while true, testing.
     #TODO: na to kanw na afairei to amaksi pou thelw. me dict extra: na mporww na kalesw to amaksi kai apo th thesi kai apo thn pinakida (logika tha xrhsimpoeisw to idio dict kai tha to kalw diaforetika)!
-    pass
+    garage = deque()
+
+
+    while True:
+        print("\nChoose an opperation:")
+        print("1. Display garage")
+        print("2. Add a car to the garage")
+        print("3. Remove a car from the garage")
+        print("4. Exit")
+
+        try:
+            choice = int(input("Please choose an option number 1 and 4: "))
+        except ValueError:
+            print("Invalid input.")
+            continue
+
+        textDelay()
+        
+        match choice:
+            case 1:
+                display_garage(garage)
+            case 2:
+                add_car_to_garage(garage, 100)
+            case 3:
+                remove_car_from_garage(garage)
+            case 4:
+                print("Goodbye")
+                break
+            case _:
+                "Invalid choice. Please try again"
+
+        textDelay()
+
+
 
 if __name__ == "__main__":
     main()
