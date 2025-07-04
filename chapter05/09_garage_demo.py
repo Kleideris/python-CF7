1e34ea8385c5223ac18c215a8f1838e60e828d13
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-04 04:36:40 +0300

    feat: refactor garage to support add and removal of cars by plate, remove typos, improve code structure

--- chapter05/09_garage_demo.py
+++ chapter05/09_garage_demo.py
@@ -1,33 +1,50 @@
 from collections import deque
-
-def display_garage(garage: deque) -> None:
+#TODO add enumerate and the ability to choose if you take out a car by plate or by number.
+def display_garage(garage: dict[str, str]) -> None:
     if garage:
         print("Current cars in the garage:")
-        for i, car in enumerate(garage, 1):
-            print(f"{i}. {car}")
+        for plate, car in garage.items():
+            print(f"Plate: {plate} — Car: {car}")
     else:
         print("The garage is empty")
 
-def add_car_to_garage(garage: deque, max_capacity: int) -> None:
-    if len(garage) < max_capacity:
-        car_name = input("Enter the name or ID of the car: ")
-        garage.append(car_name)
-        print(f"{car_name} has entered in the garage.")
-    else:
+def add_car_to_garage(garage: dict[str, str], max_capacity: int) -> None:
+    if len(garage) >= max_capacity:
         print("The garage is full. Cannot add more cars.")
 
-def remove_car_from_garage(garage: deque) -> None:
-    if garage:
-        car_left = garage.popleft()
-        print(f"{car_left} has left the garage.")
-    else:
+    user_input = input("Enter the car's plate and the name seperated by coma (ex: ABC123, Toyota Corolla): ")
+
+    try:
+        plate, car_name = map(str.strip, user_input.split(","))
+    except ValueError:
+        print("Invalid format. Please enter the plate and car name separated by a comma.")
+
+    if not plate or not car_name:
+         print("Both plate and name are required.")
+    
+    if plate in garage:
+        print(f"Plate {plate} already exists.")
+        return
+
+    garage[plate] = car_name
+    print(f"{car_name} with plate {plate} has been added.")
+   
+def remove_car_from_garage(garage: dict[str, str], plate: str) -> None:
+    if not garage:
         print("Garage is empty. No cars to remove.")
+        return
+    if plate in garage:
+        car_left = garage.pop(plate)
+        print(f"{car_left} with plate {plate} has left the garage.")
+    else:
+        print(f"There are no cars in the garage with plate '{plate}'.")        
 
 def main():
-    garage = deque()
+    garage = {"XXX123": "toyota", "YYY456": "mini cooper"}
+    max_capacity = 100
 
     while True:
-        print("\nChoose an opperation:")
+        print("\nChoose an operation:")
         print("1. Display garage")
         print("2. Add a car to the garage")
         print("3. Remove a car from the garage")
@@ -43,14 +60,15 @@ def main():
             case 1:
                 display_garage(garage)
             case 2:
-                add_car_to_garage(garage, 100)
+                add_car_to_garage(garage, max_capacity)
             case 3:
-                remove_car_from_garage(garage)
+                plate = input("Enter the plate of the car you want to remove: ").strip()
+                remove_car_from_garage(garage, plate)
             case 4:
                 print("Goodbye")
                 break
             case _:
-                "Invalid choice. Please try again"
+                print("Invalid choice. Please try again")
 
 if __name__ == "__main__":
     main()
